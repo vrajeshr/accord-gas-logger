fb994e3eab43c7325b70615582755c47a207239b
Vrajesh Rana <vur2@njit.edu>
2020-11-13 23:45:43 -0500

    Added SharedPreferences. Saving to CSV file

--- app/src/main/java/com/example/accordapp/MainActivity.kt
+++ app/src/main/java/com/example/accordapp/MainActivity.kt
@@ -1,30 +1,31 @@
 package com.example.accordapp
 
+import android.R.attr.name
 import android.content.Context
-import android.os.Build
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.Environment
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jaredrummler.android.shell.Shell
 import java.io.File
-import java.sql.Time
-import java.time.LocalDateTime
-import java.util.*
+import java.net.URL
+import java.sql.Timestamp
+
+
+var AWS_URL = "https://6w0byurle5.execute-api.us-east-1.amazonaws.com/prod/accord-lambda"
+var CSV_FILE_NAME = "gas_log.csv"
 
 class MainActivity : AppCompatActivity() {
-    final var fileName = "data.csv"
-
     lateinit var odometerInput: EditText;
     lateinit var gallonsInput: EditText;
     lateinit var pricePerGallon: EditText;
     lateinit var total: EditText;
 
     lateinit var submitButton: Button;
+    lateinit var sharedPref:SharedPreferences;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +35,8 @@ class MainActivity : AppCompatActivity() {
         pricePerGallon = findViewById(R.id.pricePerGallonInput);
         total = findViewById(R.id.totalInput);
         submitButton = findViewById(R.id.submitButton);
+
+        sharedPref = this?.getPreferences(Context.MODE_PRIVATE)
 
 
         gallonsInput.addTextChangedListener(object : TextWatcher {
@@ -68,30 +71,47 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    fun onSubmit() {
+        var line = getCSVLine();
+
+        var file = File(getExternalFilesDir("AccordApp"), CSV_FILE_NAME)
+
+        if( !file.exists() ){
+            try{
+                file.createNewFile();
+            }
+            catch ( e : Exception ){
+                println("File failed to write: ${e.toString()}")
+            }
+        }
+
+        try {
+            file.appendText(line);
+        }
+        catch( e: Exception) {
+            println("File I/O Error: $e");
+        }
+        sendPOST();
+    }
+
     fun getCSVLine(): String {
         var line = ""
-        var localTime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            LocalDateTime.now()
-        } else {
 
-        };
-        line += "$localTime,"
-        line += "${odometerInput.text.toString()},"
-        line += "${pricePerGallon.text.toString()},"
-        line += "${gallonsInput.text.toString()},"
-        line += "${total.text.toString()},"
+        var now = Timestamp(System.currentTimeMillis());
+
+        line += "$now,"
+        line += "${odometerInput.text},"
+        line += "${pricePerGallon.text},"
+        line += "${gallonsInput.text},"
+        line += "${total.text}\n"
 
         return line
     }
 
-    fun onSubmit() {
-        val file = File(this.filesDir, fileName)
-        if( !file.exists() ){
-            file.createNewFile()
-        }
+    fun sendPOST(){
+        val url = URL(AWS_URL)
 
-        var line = getCSVLine()
-
-        file.writeText(line)
     }
+
 }
